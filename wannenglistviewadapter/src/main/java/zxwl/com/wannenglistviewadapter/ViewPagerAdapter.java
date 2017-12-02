1d83b62448eaefc11d87ab2506e56fba363009fd
wyz12 <690298131@qq.com>
2017-12-02 10:35:50 +0800

    添加viewpagerAdapter适配器

---
+++ wannenglistviewadapter/src/main/java/zxwl/com/wannenglistviewadapter/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package zxwl.com.wannenglistviewadapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017/12/2.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> list;
+    private ArrayList<String> slist;
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return slist.get(position);
+    }
+}
