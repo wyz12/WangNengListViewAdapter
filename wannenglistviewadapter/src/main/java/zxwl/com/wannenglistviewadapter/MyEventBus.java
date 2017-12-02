73f51f84869dd7d18d14bb3035ec92aa4c25ae8a
wyz12 <690298131@qq.com>
2017-12-02 11:02:28 +0800

    添加viewpagerAdapter适配器

---
+++ wannenglistviewadapter/src/main/java/zxwl/com/wannenglistviewadapter/MyEventBus.java
@@ -0,0 +1,23 @@
+package zxwl.com.wannenglistviewadapter;
+
+/**
+ * Created by Administrator on 2017/12/2.
+ */
+
+public class MyEventBus {
+
+    private String ss;
+
+    public MyEventBus(String ss) {
+
+        this.ss = ss;
+
+    }
+
+    public String getSs() {
+
+        return ss;
+
+    }
+
+}
